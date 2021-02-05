70d0b53826131ff3ef2f38f315dc8eeada57aa72
Alexander Popov <rastopyakin@gmail.com>
2021-02-06 02:20:31 +0700

    Test reproc

--- tester/main.cpp
+++ tester/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <filesystem>
+#include <system_error>
+#include <tuple>
+#include <vector>
+
+#include "reproc++/reproc.hpp"
+#include "reproc++/run.hpp"
 
 int main(int argc, char *argv[]) {
+
+    reproc::process process;
+
+    int status = 0;
+    std::error_code ec;
+    reproc::options options;
+    options.redirect.parent = true;
+
+
+    std::tie(status, ec) =  reproc::run(argv + 1, options);
 
     return 0;
 }
