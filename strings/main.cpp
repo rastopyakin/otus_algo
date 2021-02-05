8b4c125e459b94e4df24561ec0a0d599707127fe
Alexander Popov <rastopyakin@gmail.com>
2021-02-05 20:44:07 +0700

    Add strings task and tester utility project

---
+++ strings/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+
+    std::string str;
+
+    std::getline(std::cin, str);
+    std::cout << str.size() - 1 << std::endl;
+    return 0;
+}
---
+++ tester/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <filesystem>
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
