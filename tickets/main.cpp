e508aeb69b62c8b5bdbe0f93f5daba695bfa3544
Alexander Popov <rastopyakin@gmail.com>
2021-02-16 16:42:42 +0700

    Remove dead code

--- tickets/main.cpp
+++ tickets/main.cpp
@@ -14,29 +14,6 @@ void tickets_naive1(int N, int s1, int s2) {
       tickets_naive1(N - 1, s1 + i, s2 + j);
 }
 
-class sums {
-public:
-  uint64_t count(int n_digits, int sum) {
-    counter = 0;
-    number_impl(n_digits, sum, 0);
-    return counter;
-  }
-
-private:
-  uint64_t counter;
-  void number_impl(int N, const int &s, int s1) {
-
-    if (N == 0) {
-      if (s1 == s) {
-        counter++;
-      }
-      return;
-    }
-    for (int i = 0; i < 10; i++)
-      number_impl(N-1, s, s1+i);
-  }
-};
-
 uint64_t count(int n_digits, int sum) {
   uint64_t counter = 0;
   if (n_digits == 1)
