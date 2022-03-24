974b3f149c7e362f5af960fd202ad9b5f2c7fa13
Alexander Popov <rastopyakin@gmail.com>
2022-03-25 05:43:54 +0700

    Better structured displaying b_tree

--- b_tree/b_tree.hpp
+++ b_tree/b_tree.hpp
@@ -15,7 +15,6 @@ template <class T> class BSTree {
   struct node {
     node(T t) : payload{t} {}
     T payload;
-    node *parent = nullptr;
     std::unique_ptr<node> left, right;
   };
 
@@ -33,10 +32,8 @@ private:
       return std::make_unique<node>(t);
     if (t < _root->payload) {
       _root->left = insert(t, _root->left);
-      _root->left->parent = _root.get();
     } else {
       _root->right = insert(t, _root->right);
-      _root->right->parent = _root.get();
     }
     return std::move(_root);
   }
@@ -45,12 +42,7 @@ private:
     if (!_root)
       return;
     traverse(_root->left.get());
-    std::cout << _root->payload << ", parent: ";
-    if (_root->parent)
-      std::cout << _root->parent->payload;
-    else
-      std::cout << "null";
-    std::cout << "\n";
+    std::cout << _root->payload << "\n";
     traverse(_root->right.get());
   }
 
@@ -87,8 +79,6 @@ bool check_positions(node_pos_t<T> *p_l, node_pos_t<T> *p_r) {
   if (need_fix) {
 
     int shift = p_l->position + node_width(*p_l) - p_r->position;
-    // std::cout << "shift: " << shift << ", node: " << p_r->payload;
-    // std::cout << " ,l: " << p_r->l->payload << "\n";
 
     node_pos_t<T> *root_to_shift = p_r;
     while (root_to_shift->parent->l == root_to_shift) {
@@ -114,6 +104,7 @@ bool check_level(int level, Cont<node_pos_t<T>> &positions) {
 
   auto l_beg = std::find_if(positions.begin(), positions.end(), is_lvl);
 
+  // TODO: potential bug here: l_end will point to smth even if nothing found
   auto l_end = --std::find_if_not(l_beg, positions.end(), is_lvl);
 
   bool stable = true;
@@ -122,6 +113,25 @@ bool check_level(int level, Cont<node_pos_t<T>> &positions) {
     if (!check_positions(&(*l_beg), &(*std::next(l_beg))))
       stable = false;
     l_beg++;
+  }
+
+  return stable;
+}
+
+template <template <class> class Cont, class T>
+bool check_overlaps(Cont<node_pos_t<T>> &positions) {
+
+  bool stable = true;
+  if (std::distance(std::begin(positions), std::end(positions)) < 4)
+    return true;
+
+  for (auto i = positions.begin(); i != std::prev(positions.end());
+       i++) {
+    if (auto next = std::next(i); next->l && (next->level == i->level))
+      stable = check_positions(&(*i), &(*next->l));
+
+    if (auto next = std::next(i); i->r && (next->level == i->level))
+      check_positions(&(*i->r), &(*next));
   }
 
   return stable;
@@ -174,17 +184,16 @@ template <class T> void BSTree<T>::display() const {
       positions.back().r = &pos_queue.back();
     }
 
-    for (int level = pos.level; level > 0; level--)
+    for (int level = pos.level; level > 0; level--) {
       check_level(level, positions);
+    }
     check_overlaps(positions);
   }
 
-  // bool stable = false;
-  // while (!stable) {
-  // for (int level = positions.back().level; level > 0; level--)
-  // stable = check_level(level, positions);
-  // stable = check_overlaps(positions);
-  // }
+  for (int level = positions.back().level; level > 0; level--) {
+    check_level(level, positions);
+    check_overlaps(positions);
+  }
 
   int min_pos =
       std::min_element(positions.begin(), positions.end(),
@@ -196,58 +205,6 @@ template <class T> void BSTree<T>::display() const {
   }
 
   draw(positions);
-}
-
-template <template <class> class Cont, class T>
-bool check_overlaps(Cont<node_pos_t<T>> &positions) {
-
-  bool stable = true;
-  for (auto i = std::next(positions.begin()); i != std::prev(positions.end());
-       i++) {
-    if (auto prev = std::prev(i); i->l && (prev->level == i->level))
-      if (i->l->position <= prev->position + node_width(*prev)) {
-        // std::cout << prev->payload << " ";
-        int shift = prev->position + node_width(*prev) - i->l->position;
-        node_pos_t<T> *root_to_shift = &(*i);
-        while (root_to_shift->parent->l == root_to_shift) {
-          root_to_shift = root_to_shift->parent;
-        }
-
-        shift_positions(shift / 2 + 1, root_to_shift);
-
-        root_to_shift = &(*prev);
-        while (root_to_shift->parent->r == root_to_shift) {
-          root_to_shift = root_to_shift->parent;
-        }
-
-        shift_positions(-shift / 2 - 1, root_to_shift);
-
-        stable = false;
-      }
-
-    if (auto next = std::next(i); i->r && (next->level == i->level))
-      if (i->r->position + node_width(*i->r) >= next->position) {
-        // std::cout << next->payload << " ";
-        int shift = i->r->position + node_width(*i->r) - next->position;
-        node_pos_t<T> *root_to_shift = &(*next);
-        while (root_to_shift->parent->l == root_to_shift) {
-          root_to_shift = root_to_shift->parent;
-        }
-
-        shift_positions(shift / 2 + 1, root_to_shift);
-
-        root_to_shift = &(*i);
-        while (root_to_shift->parent->r == root_to_shift) {
-          root_to_shift = root_to_shift->parent;
-        }
-
-        shift_positions(-shift / 2 - 1, root_to_shift);
-        stable = false;
-      }
-  }
-
-  return stable;
-  std::cout << "\n";
 }
 
 template <template <class> class Cont, class T>
